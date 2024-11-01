728816f4ae48408bda4383f56049086a515fd8d1
rileypatrickmartin <rmartin@gatech.edu>
2024-10-31 20:57:10 -0400

    Parent node work

--- src/parentnode.py
+++ src/parentnode.py
@@ -10,11 +10,5 @@ class ParentNode(HTMLNode):
             raise ValueError
         if self.children is None:
             raise ValueError("There are no children!")
-        # Otherwise, 
-        # return a string representing the HTML tag of the node and its children. 
-        # This should be a recursive method 
-        #  (each recursion being called on a nested child node).
-        # I iterated over all the children and called to_html on each,
-        #  concatenating the results and injecting them 
-        #  between the opening and closing tags of the parent.
+
         return f"<{self.tag}>{''.join([node.to_html() for node in self.children])}</{self.tag}>"
